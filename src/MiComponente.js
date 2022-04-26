650a8620c8d0cd2f0f498c6cb4052fafce54fd59
Dani <danielmateu86@gmail.com>
2022-04-26 16:24:47 +0200

    Primeros pasos con Rreact

---
+++ src/MiComponente.js
@@ -0,0 +1,31 @@
+// Importar modulos de react/dependencias
+import React, {Fragment} from 'react'
+
+//Funcion del COmponente
+const MiComponente = () =>{
+
+    return (
+    <Fragment>
+        
+    <h2>Componente Creado</h2>
+    <p>Este es mi primer componente</p>
+    <ul>
+        <li>
+            JS
+            </li>
+        <li>
+            Angular
+            </li>
+        <li>
+            React
+            </li>
+    </ul>
+    </Fragment>
+    
+    
+    )
+}
+
+
+//Export
+export default MiComponente;

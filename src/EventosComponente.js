ff647aebf8c06bce11ed7016f94be4d1320f9b92
Dani <danielmateu86@gmail.com>
2022-04-26 18:11:50 +0200

    Evento DoubleClick

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -5,13 +5,22 @@ export const EventosComponente = () => {
     const hasDadoClick = (e,nombre) =>{
         alert(`Has dado Click ${nombre}`);
     }
+    const dobleClick = (e) =>{
+        alert('Has dado Doble Click')
+    }
 
   return (
     <div>
         <h1>Eventos En React</h1>
-        {/* Evento Click */}
-    <button onClick={e => hasDadoClick(e,'Dani')}>Dame Click!</button>
 
+        <p>
+             {/* Evento Click */}
+            <button onClick={e => hasDadoClick(e,'Dani')}>Dame Click!</button>
+        </p>
+        <p>
+            {/* Evento Doble Click */}
+            <button onDoubleClick={e => dobleClick(e)}>Dame Doble Click!</button>
+        </p>
     </div>
   )
 }

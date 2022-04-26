6374b38c3608f39fda37a6249ca08b3325200dde
Dani <danielmateu86@gmail.com>
2022-04-26 18:24:19 +0200

    Evento mouseEnter y mouseLeave

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -9,10 +9,13 @@ export const EventosComponente = () => {
         alert('Has dado Doble Click')
     }
 
+    const hasEntrado = (e,accion) =>{
+        alert(`Has ${accion} a la caja con el mouse!`);
+    }
+
   return (
     <div>
         <h1>Eventos En React</h1>
-
         <p>
              {/* Evento Click */}
             <button onClick={e => hasDadoClick(e,'Dani')}>Dame Click!</button>
@@ -21,6 +24,13 @@ export const EventosComponente = () => {
             {/* Evento Doble Click */}
             <button onDoubleClick={e => dobleClick(e)}>Dame Doble Click!</button>
         </p>
+
+        <div id="caja" 
+        onMouseEnter={e => hasEntrado(e,'entrado')}
+        onMouseLeave={e => hasEntrado(e,'salido')}>
+            {/* Evento onMouseEnter onMouseLeave */}
+            Pasa por encima!
+        </div>
     </div>
   )
 }
